816d4131f582cb00c8750806da55b7e6df654e9e
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2024-12-24 14:53:32 -0500

    day 24 pt2 (not done yet)

--- Aoc24/Day24.lean
+++ Aoc24/Day24.lean
@@ -131,6 +131,99 @@ PART 2:
 
 -- 312 wires, 222 gates, x and y are 44 bits each
 
+inductive Role where
+| xinput (n : Nat) : Role
+| yinput (n : Nat) : Role
+| xorlevel1 (n : Nat) : Role   -- xn XOR yn  (except z00, which is an output)
+| andlevel1 (n : Nat) : Role   -- xn AND yn
+| andlevel2 (n : Nat) : Role   -- (xn XOR yn) AND carry (n-1)
+| output (n : Nat) : Role   -- (xn XOR yn) XOR carry (n-1)  (= zn)
+| carry (n : Nat) : Role    -- only OR gates
+deriving DecidableEq, Hashable, Repr, Inhabited
+
+partial def lightcone (graph : Std.HashMap Wire Gate) (v : Wire) : StateM (Std.HashSet Wire) Unit := do
+  if graph.contains v then
+    set <| (← get).insert v
+    let g := graph[v]!
+    lightcone graph g.op1
+    lightcone graph g.op2
+  else
+    set <| (← get).insert v
+
+def Gate.toString (g : Gate) : String :=
+  match g with
+  | .xor a b c => s!"{String.ofCharArray a} XOR {String.ofCharArray b} -> {String.ofCharArray c}"
+  | .and a b c => s!"{String.ofCharArray a} AND {String.ofCharArray b} -> {String.ofCharArray c}"
+  | .or a b c =>  s!"{String.ofCharArray a} OR {String.ofCharArray b} -> {String.ofCharArray c}"
+
+def printPartialGraph (graph : Std.HashMap Wire Gate) (wires : Std.HashSet Wire) : IO Unit := do
+  for w in wires do
+    match graph[w]? with
+    | none => noop
+    | some g => IO.println g.toString
+
+def getXorLevel1 (graph : Std.HashMap Wire Gate) : Std.HashMap Wire Role := Id.run do
+  let mut out : Std.HashMap Wire Role := .empty
+  for v in graph.keys do
+    match graph[v]! with
+    | .xor a b c =>
+        if (String.ofCharArray c) != "z00" then
+          match a[0]!, b[0]! with
+          | 'x', 'y' =>
+              let xnum := ((String.ofCharArray a).drop 1).toNat!
+              let ynum := ((String.ofCharArray b).drop 1).toNat!
+              if xnum != ynum then panic! "level one with mismatched numbers"
+              out := out.insert v (.xorlevel1 xnum)
+          | 'y', 'x' =>
+              let xnum := ((String.ofCharArray b).drop 1).toNat!
+              let ynum := ((String.ofCharArray a).drop 1).toNat!
+              if xnum != ynum then panic! "level one with mismatched numbers"
+              out := out.insert v (.xorlevel1 xnum)
+          | _, _ => noop
+    | _ => noop
+  return out
+
+def getAndLevel1 (graph : Std.HashMap Wire Gate) (roles : Std.HashMap Wire Role := .empty) : Std.HashMap Wire Role := Id.run do
+  let mut out : Std.HashMap Wire Role := roles
+  for v in graph.keys do
+    match graph[v]! with
+    | .and a b c =>
+        if (String.ofCharArray c) != "mcg" then
+          match a[0]!, b[0]! with
+          | 'x', 'y' =>
+              let xnum := ((String.ofCharArray a).drop 1).toNat!
+              let ynum := ((String.ofCharArray b).drop 1).toNat!
+              if xnum != ynum then panic! "level one with mismatched numbers"
+              out := out.insert v (.andlevel1 xnum)
+          | 'y', 'x' =>
+              let xnum := ((String.ofCharArray b).drop 1).toNat!
+              let ynum := ((String.ofCharArray a).drop 1).toNat!
+              if xnum != ynum then panic! "level one with mismatched numbers"
+              out := out.insert v (.andlevel1 xnum)
+          | _, _ => noop
+        else out := out.insert v (.carry 0)
+    | _ => noop
+  return out
+
+def getAndLevel2 (graph : Std.HashMap Wire Gate) (roles : Std.HashMap Wire Role := .empty) : Std.HashMap Wire Role := Id.run do
+  let mut out : Std.HashMap Wire Role := roles
+  for v in graph.keys do
+    match graph[v]! with
+    | .and a b c =>
+          match roles[a]?, roles[b]? with
+          | some (.xorlevel1 n), some (.carry m) =>
+              if n != m+1 then panic! s!"and level two with mismatched numbers: {a} {b} {c}"
+              out := out.insert v (.andlevel2 n)
+          | r, some (.xorlevel1 n) =>
+              match r with
+              | some (.carry m) =>
+                if n != m+1 then panic! s!"and level two with mismatched numbers: {a} {b} {c}"
+                out := out.insert v (.andlevel2 n)
+              | _ => noop
+          | _, _ => noop
+    | _ => noop
+  return out
+
 def secondPart (input : FilePath) : IO String := do
   let some ⟨initwires, gates⟩ := (← IO.FS.readFile input).parse? parseInput | IO.exitWithError "Parse error"
   let graph : Std.HashMap Wire Gate := gates.foldl (init := .empty) fun acc cur =>
@@ -152,11 +245,19 @@ def secondPart (input : FilePath) : IO String := do
       let aval := wirevals[g.op1]!
       let bval := wirevals[g.op2]!
       wirevals := wirevals.insert w (g.eval aval bval)
-  IO.println gates.size
+  let lc00 := (lightcone graph "z00".toCharArray).runState .empty
+  let lc01 := (lightcone graph "z01".toCharArray).runState .empty
+  let lc02 := (lightcone graph "z02".toCharArray).runState .empty
+
+  let mut roles := getXorLevel1 graph
+  roles := getAndLevel1 graph roles
+  roles := getAndLevel2 graph roles
+  IO.print (repr roles)
+  IO.println roles.size
   return "foo"
 
 --#eval secondPart testinput1           --(ans: )
 --#eval secondPart testinput2           --(ans: )
---#eval secondPart realinput           --(ans: )
+#eval secondPart realinput           --(ans: )
 
 end Day24
